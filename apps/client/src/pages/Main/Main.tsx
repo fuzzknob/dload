350e38c32d6bacec81b90022ca874658b5248800
Gagan Rai <gagan_rai@outlook.com>
2023-04-27 17:59:35 +0545

    feat: hook up task list to show live tasks

--- apps/client/src/pages/Main/Main.tsx
+++ apps/client/src/pages/Main/Main.tsx
@@ -4,12 +4,18 @@ import { Space, Group, Box, Title, ActionIcon } from '@mantine/core'
 import { useHotkeys } from '@mantine/hooks'
 
 import MainLayout from '@/layouts/MainLayout'
+import { useTaskStore } from '@/modules/tasks/task-store'
 
 import Task from './components/Task'
 import AddTask from './components/AddTask'
 
 const Main: React.FC = () => {
   const [isTaskVisible, setTaskVisible] = useState(false)
+  const tasks = useTaskStore((state) => state.tasks)
+  const activeTasks = tasks.filter((task) =>
+    ['DOWNLOAD', 'COPY'].includes(task.type),
+  )
+  const queuedTasks = tasks.filter((task) => task.type === 'QUEUE')
 
   useHotkeys([['n', () => setTaskVisible(true)]])
 
@@ -32,14 +38,17 @@ const Main: React.FC = () => {
       <Box>
         <Title order={3}>In Progress</Title>
         <Space h="md" />
-        <Task type="DOWNLOAD" />
-        <Task type="COPY" />
+        {activeTasks.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
       </Box>
       <Space h={50} />
       <Box>
         <Title order={3}>In Queue</Title>
         <Space h="md" />
-        <Task type="QUEUE" />
+        {queuedTasks.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
       </Box>
     </MainLayout>
   )
