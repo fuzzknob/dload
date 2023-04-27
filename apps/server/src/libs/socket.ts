02695a2b73c7aa94a225aabda5daa81768c913e1
Gagan Rai <gagan_rai@outlook.com>
2023-04-27 18:00:04 +0545

    refactor: export everything socket.io

--- apps/server/src/libs/socket.ts
+++ apps/server/src/libs/socket.ts
@@ -1,7 +1,10 @@
 import { Server } from 'socket.io'
+
 export const io = new Server({
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 })
+
+export * from 'socket.io'
