505af8cec3d8adf85f6fc5a24d980fa3029c926a
Gagan Rai <gagan_rai@outlook.com>
2023-04-27 18:02:28 +0545

    feat: add Task add socket event

--- apps/server/src/modules/tasks/task-controller.ts
+++ apps/server/src/modules/tasks/task-controller.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { io } from '@/libs/socket'
+import { SOCKET_EVENTS } from '@dload/shared'
 import * as taskStore from './task-store'
+import * as taskService from './task-service'
 
 const router = Router()
 
 io.on('connection', (socket) => {
   const tasks = taskStore.allTasks()
-  socket.emit('TASK_UPDATE', tasks)
+  socket.emit(SOCKET_EVENTS.TASK_UPDATE, Object.values(tasks))
+  socket.on(SOCKET_EVENTS.ADD_TASK, async (task: taskStore.Task) => {
+    await taskService.addTask(task)
+  })
 })
 
 export default router
